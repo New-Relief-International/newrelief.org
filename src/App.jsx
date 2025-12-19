5c8230cd0371256eb06fbe542ffeb46572a8c661
Jeffery Marfo <marfojeffery31@gmail.com>
2025-12-19 17:14:26 +0000

    Added vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RootLayout from './layouts/RootLayout';
 import Landing from './Pages/Landing';
 import Messages from './Pages/Messages';
